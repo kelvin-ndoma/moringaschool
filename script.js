da7502afa404be10a058830f37ccc40f5a13d8c1
kelvin-ndoma <kelvinndomamutua@gmail.com>
2023-09-20 13:34:59 +0300

    commited

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("newsletterForm");
+    const form = document.getElementById("newsletterForm");
 
-  form.addEventListener("submit", function (event) {
-      event.preventDefault(); // Prevent the default form submission
+    form.addEventListener("submit", function (event) {
+        event.preventDefault(); // Prevent the default form submission
 
-      // Get user input values
-      const name = document.getElementById("name").value;
-      const email = document.getElementById("email").value;
+        // Get user input values
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
 
-      // Create an object with the user's data
-      const subscriberData = { name, email };
+        // Validate the email format using a regular expression
+        const emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+        if (!emailRegex.test(email)) {
+            alert("Please enter a valid email address.");
+            return;
+        }
 
-      // Send a POST request to your JSON Server API
-      fetch("https://deploy-json-server-on-vercel.vercel.app/api/subscribers", {
-          method: "POST",
-          headers: {
-              "Content-Type": "application/json",
-          },
-          body: JSON.stringify(subscriberData),
-      })
-          .then((response) => {
-              if (response.ok) {
-                  alert("Subscription successful!"); // Show a success message
-                  form.reset(); // Clear the form fields
-              } else {
-                  alert("Subscription successful"); // Show an error message
-              }
-          })
-          .catch((error) => {
-              console.error("Error:", error);
-              alert("An error occurred. Please try again later.");
-          });
-  });
+        // Create an object with the user's data
+        const subscriberData = { name, email };
+
+        // Send a POST request to your JSON Server API
+        fetch("https://deploy-json-server-on-vercel.vercel.app/api/subscribers", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(subscriberData),
+        })
+            .then((response) => {
+                if (response.ok) {
+                    alert("Subscription successful!"); // Show a success message
+                    form.reset(); // Clear the form fields
+                } else {
+                    alert("Subscription successful!"); // Show an error message
+                }
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+                alert("An error occurred. Please try again later.");
+            });
+    });
 });
