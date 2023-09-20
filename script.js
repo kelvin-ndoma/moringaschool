6186348facd17503cd88c4074d8e75913c799a94
kelvin-ndoma <kelvinndomamutua@gmail.com>
2023-09-20 13:10:37 +0300

    created a landing page

--- script.js
+++ script.js
@@ -1,48 +1,38 @@
-// Select the form element and add an event listener for form submission
-const form = document.getElementById('newsletterForm');
-form.addEventListener('submit', (e) => {
-  e.preventDefault(); // Prevent the default form submission behavior
+// script.js
 
-  // Get the user input from the form fields
-  const nameInput = document.getElementById('name');
-  const emailInput = document.getElementById('email');
-  const userData = {
-    name: nameInput.value,
-    email: emailInput.value,
-    id: generateUserId(), // You can generate a unique ID here
-  };
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("newsletterForm");
+  const subscribeBtn = document.getElementById("subscribeBtn");
 
-  // Make a POST request to the server
-  fetch('https://digitalmarketing-d5jppwmih-kelvin-ndoma.vercel.app/subscribers', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(userData),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        // Handle the error response from the server
-        return response.json().then((errorData) => {
-          throw new Error(errorData.error);
-        });
-      }
-      return response.json();
-    })
-    .then((data) => {
-      // Handle the success response here
-      console.log(data);
-      // You can also update your UI to indicate success to the user
-    })
-    .catch((error) => {
-      // Handle the error and display it to the user
-      console.error(error.message);
-      // You can update your UI to display the error message to the user
-    });
+  form.addEventListener("submit", function (event) {
+      event.preventDefault(); // Prevent the default form submission
+
+      // Get user input values
+      const name = document.getElementById("name").value;
+      const email = document.getElementById("email").value;
+
+      // Create an object with the user's data
+      const subscriberData = { name, email };
+
+      // Send a POST request to your JSON Server API
+      fetch("/api/subscribers", {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify(subscriberData),
+      })
+          .then((response) => {
+              if (response.ok) {
+                  alert("Subscription successful!"); // Show a success message
+                  form.reset(); // Clear the form fields
+              } else {
+                  alert("Subscription failed. Please try again."); // Show an error message
+              }
+          })
+          .catch((error) => {
+              console.error("Error:", error);
+              alert("An error occurred. Please try again later.");
+          });
+  });
 });
-
-// Function to generate a unique user ID (you can replace this with your logic)
-function generateUserId() {
-  // Generate a unique ID here and return it
-  return 'uniqueId123';
-}
